c0ce3dc7024b33c1b716203fb42e81d46a5c66f5
SOCOM-CMD <61659602+SOCOM-CMD@users.noreply.github.com>
2020-03-01 15:35:50 -0500

    Update pagehandler.js

--- pagehandler.js
+++ pagehandler.js
@@ -6,6 +6,8 @@ window.onload = function(){
 	input.onkeydown = function() {
 		var ev = event.charCode;
 		var res = String.fromCharCode(ev);
+		if(res.length > 0)
+			console.log("made it here boioii");
 		console.log(res);
 		var key = event.keyCode || event.charCode;
 

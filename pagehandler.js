b84724a7fb06a518d873ea1a489c52a17c507128
SOCOM-CMD <61659602+SOCOM-CMD@users.noreply.github.com>
2020-03-01 14:18:08 -0500

    Update pagehandler.js

--- pagehandler.js
+++ pagehandler.js
@@ -5,11 +5,10 @@ window.onload = function(){
 
 	input.onkeydown = function() {
 		var key = event.keyCode || event.charCode;
+		console.log(key);
 
 		if( key == 8 || key == 46 )
 			return false;
-		
-		console.log(key);
 	};
 
 }
